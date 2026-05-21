d6d6cf007b7a2b268960f9dd58f5a691b7f7b5ca
Nishat-Annan-Mim <nishat.annan.mim.2003@gmail.com>
2026-05-21 14:46:20 +0600

    bookconfirm

--- src/components/Spinner.jsx
+++
@@ -1,6 +0,0 @@
-const Spinner = () => (
-  <div className="flex justify-center items-center min-h-[40vh]">
-    <div className="w-12 h-12 rounded-full border-4 border-primary border-t-transparent animate-spin"></div>
-  </div>
-);
-export default Spinner;
